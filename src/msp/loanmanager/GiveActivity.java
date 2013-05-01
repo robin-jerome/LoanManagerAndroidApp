d4080e1bb61724a4856e5c09c05ee89d4cfb8710
robinbj86 <robin.jerome@outlook.com>
2013-05-01 15:59:49 +0300

    Commiting give activity files

---
+++ src/msp/loanmanager/GiveActivity.java
@@ -0,0 +1,65 @@
+package msp.loanmanager;
+
+import msp.action.DataHandler;
+import msp.object.Loan;
+import msp.object.Person;
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class GiveActivity extends Activity {
+
+	private DataHandler handler = new DataHandler();
+	private String fileName = MainActivity.PATH + "l";
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_person);
+          
+        
+		Button add = (Button)findViewById(R.id.add_person_confirm);
+		
+		add.setOnClickListener(new View.OnClickListener() {
+		     @Override
+		     public void onClick(View v) {
+		    	 Loan loan = new Loan();
+		    	 int loanId;
+		    	 if (MainActivity.loans.size() == 0){
+		    		 loanId = 1000;
+		    	 }else{
+		    		 loanId = MainActivity.loans.get(MainActivity.loans.size()-1).getId() + 1;		    		
+		    	 }
+		    	 loan.setId(loanId);
+		    	 
+		    	 EditText personName = (EditText)findViewById(R.id.add_person_name);
+		    	 EditText itemName = (EditText)findViewById(R.id.add_item_name);
+		    	 EditText categoryName = (EditText)findViewById(R.id.add_category_name);
+		    	 EditText itemCount = (EditText)findViewById(R.id.add_item_count);
+		    	 EditText loanDate = (EditText)findViewById(R.id.add_loan_date);
+		    	 EditText dueDate = (EditText)findViewById(R.id.add_due_date);
+		    	 EditText amount = (EditText)findViewById(R.id.add_amount);
+		    	 
+		    	 loan.setAmount(Integer.valueOf(amount.getText().toString()));
+		    	 
+			     Intent intent = new Intent(GiveActivity.this, MainActivity.class);	
+			     startActivity(intent);
+		     }
+		 });
+
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.add_person, menu);
+        return true;
+	}
+
+}
