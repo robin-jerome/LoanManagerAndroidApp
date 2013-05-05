c783ac9369a7abb049cabeaab25822fe7f1dc37b
robinbj86 <robin.jerome@outlook.com>
2013-05-05 11:39:47 +0300

    Added classes to action

---
+++ src/msp/action/CustomOnItemSelectedListener.java
@@ -0,0 +1,23 @@
+package msp.action;
+
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.Toast;
+
+public class CustomOnItemSelectedListener implements OnItemSelectedListener {
+
+	public void onItemSelected(AdapterView<?> parent, View view, int pos,
+			long id) {
+		System.out.println("Something Important::"+parent.getItemAtPosition(pos).toString());
+		Toast.makeText(parent.getContext(), 
+				"OnItemSelectedListener : " + parent.getItemAtPosition(pos).toString(),
+				Toast.LENGTH_SHORT).show();
+	}
+
+	@Override
+	public void onNothingSelected(AdapterView<?> arg0) {
+		// TODO Auto-generated method stub
+	}
+
+}
---
+++ src/msp/action/Util.java
@@ -0,0 +1,33 @@
+package msp.action;
+
+import java.util.ArrayList;
+
+import msp.loanmanager.MainActivity;
+import msp.object.Group;
+import msp.object.Person;
+
+public class Util {
+
+	public static int getPersonIdFromName(String personName) {
+		int retVal = -1;
+		ArrayList<Person> personList = MainActivity.persons;
+		for(Person person: personList){
+			if(personName.equals(person.getName())){
+				retVal = person.getId();
+			}
+		}
+		return retVal;
+	}
+
+	public static int getGroupIdFromName(String groupName) {
+		int retVal = -1;
+		ArrayList<Group> groupList = MainActivity.groups;
+		for(Group group: groupList){
+			if(groupName.equals(group.getGroupName())){
+				retVal = group.getId();
+			}
+		}
+		return retVal;
+	}
+	
+}
